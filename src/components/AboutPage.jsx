a2f3f69976e54cd9a04cf7fed49367a5e53be17a
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2024-10-20 12:15:01 -0600

    added img to about page

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -4,6 +4,12 @@ function AboutPage() {
   return (
     <div className="About">
       <div className="header">
+        <img
+          alt="about-logo"
+          src="https://www.clipartbest.com/cliparts/9cz/pKb/9czpKbdbi.jpg"
+          width={200}
+          height={200}
+        ></img>
         <p id="about-paragraph">
           This project utilizes react.js and live API calls to display an NBA
           player roster for each NBA team. <br />
