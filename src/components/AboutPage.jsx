d52ce7094d519d04c0f6098b2b0918497686e9d5
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2024-10-20 03:03:47 -0600

    styled paragraph in about page

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -4,8 +4,11 @@ function AboutPage() {
   return (
     <div className="About">
       <div className="header">
-        This project utilizes react.js and live API calls to display an NBA
-        player roster for each NBA team. Created by: Mohamad Elgendy &#128540;
+        <p id="about-paragraph">
+          This project utilizes react.js and live API calls to display an NBA
+          player roster for each NBA team. <br />
+          Created by: Mohamad Elgendy &#128540;
+        </p>
       </div>
     </div>
   );
