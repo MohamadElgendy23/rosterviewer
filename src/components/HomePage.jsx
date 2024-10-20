710e095d25ca606b5170ed2bf2a2cb0df9c11d01
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2024-10-20 17:58:44 -0600

    removed unutilized replace method

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { replace, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./HomePage.css";
 
 function HomePage() {
